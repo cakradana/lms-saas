0ee3abbcb3fd59b7be67e204a293f94a5efcf565
cakradana <rcakradana@gmail.com>
2025-06-04 15:43:00 +0700

    Add companion creation form with validation
    Introduces a companion creation form with input fields for name, subject, topic, voice, style, and duration, leveraging React Hook Form and Zod for validation.
    
    Adds reusable UI components, including input, select, label, and textarea, to support form functionality. Updates dependencies to include necessary packages for form handling and UI enhancements.

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,14 @@
+import CompanionForm from "@/components/CompanionForm";
+
 const NewCompanion = () => {
-  return <div>NewCompanion</div>;
+  return (
+    <main className="items-center justify-center min-md:w-2/3 min-lg:w-1/3">
+      <article className="flex w-full flex-col gap-4">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
+  );
 };
 
 export default NewCompanion;
